6d62071da0458059ffd3f9e2d7b56ee33a77bfbf
Abraham Castro <abraham.castro@gmail.com>
2023-02-02 19:07:51 -0300

    mas ajustes

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -93,7 +93,7 @@ export const AppBar = ({ pages }) => {
             component="div"
             sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}
           >
-            React Router Auth
+            SAT
           </Typography>
           <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
             {pages?.map((page) => (
