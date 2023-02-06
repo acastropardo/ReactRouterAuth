114933dbeacb99e4a4b6e8e326ef4f3b770437b3
Abraham Castro <abraham.castro@gmail.com>
2023-02-06 12:44:53 -0300

    ajustes

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,17 +1,38 @@
 import { createContext, useContext, useMemo } from "react";
 import { useNavigate } from "react-router-dom";
 import { useLocalStorage } from "./useLocalStorage";
+import client from "../feathers";
+import { useState } from "react";
 
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useLocalStorage("user", null);
+  const [error, setError] = useState();
+  
   const navigate = useNavigate();
 
-  const login = async (data) => {
-    setUser(data);
-    navigate("/dashboard/clientes", { replace: true });
-  };
+  const login = async (data) =>{
+    let email = data.email;
+    let password = data.password;
+
+    client
+      .authenticate({
+        strategy: "local",
+        email,
+        password,
+      })
+      .catch((err) => setError(err));
+    
+      setUser(data);
+      navigate("/dashboard/clientes", { replace: true });
+    
+  }
+
+  // const login = async (data) => {
+  //   setUser(data);
+  //   navigate("/dashboard/clientes", { replace: true });
+  // };
 
   const logout = () => {
     setUser(null);
@@ -22,7 +43,7 @@ export const AuthProvider = ({ children }) => {
     () => ({
       user,
       login,
-      logout
+      logout,
     }),
     [user]
   );
