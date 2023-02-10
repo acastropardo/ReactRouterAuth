c8d5d1b2ec6a91b94e4437bf5c40c50f179747e1
Abraham Castro <abraham.castro@gmail.com>
2023-02-10 14:07:16 -0300

    ajustando para crear en el crud

--- src/pages/OrdenesVenta.jsx
+++ src/pages/OrdenesVenta.jsx
@@ -12,20 +12,7 @@ import SearchBar from "@mkyy/mui-search-bar";
 import Button from "@mui/material/Button";
 import { useNavigate } from "react-router-dom";
 
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
-}
-
 var rows = [];
-
-/* var rows = [
-   createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-   createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-   createData('Eclair', 262, 16.0, 24, 6.0),
-   createData('Cupcake', 305, 3.7, 67, 4.3),
-   createData('Gingerbread', 356, 16.0, 49, 3.9),
- ];
-*/
 
 export const OrdenesVentaPage = () => {
   //rows = leerOrdenes();
@@ -80,7 +67,7 @@ export const OrdenesVentaPage = () => {
     //return ordenesLista;
   }
   const [ordenesVenta, getOrdenesVenta] = useState([]);
-  const [textFieldValue, setTextFieldValue] = useState("Barra de búsqueda");
+  const [textFieldValue, setTextFieldValue] = useState("");
   
   const navigate = useNavigate();
   const handleSubmit = (event) => {
