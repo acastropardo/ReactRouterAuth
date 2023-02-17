0a43fce8944cabdd6074697b67a69cf211ea0946
acastropardo <abraham.castro@gmail.com>
2023-02-17 11:55:38 -0300

    iniciando logica para actualizar tabla dinamica

--- src/pages/OrdenesVentaGestion.jsx
+++ src/pages/OrdenesVentaGestion.jsx
@@ -36,17 +36,22 @@ import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 
 export const OrdenesVentaGestionPage = () => {
-  function createData(name, calories, fat, carbs, protein) {
-    return { name, calories, fat, carbs, protein };
+
+  const [detalleOrdenVenta, setDetalleOrdenVenta] = useState([]);
+
+  function createData(num_serie, descripcion, cantidad, fecha_alta, fecha_ultima_mantencion, observacion_1, observacion_2, observacion_3,monto,unidadMedidaId, caregoriumId, marcaId, tipoMaterialId, centrosCostoId, tipoRequerimientoId, ordenVentumId, statusOrdenVentumId) {
+    return { num_serie, descripcion, cantidad, fecha_alta, fecha_ultima_mantencion, observacion_1, observacion_2, observacion_3,monto,unidadMedidaId, caregoriumId, marcaId, tipoMaterialId, centrosCostoId, tipoRequerimientoId, ordenVentumId, statusOrdenVentumId };
   }
 
   const rows = [
-    createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
-    createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
-    createData("Eclair", 262, 16.0, 24, 6.0),
-    createData("Cupcake", 305, 3.7, 67, 4.3),
-    createData("Gingerbread", 356, 16.0, 49, 3.9),
+    createData("ABC123", "Prueba", 1, "2022-01-01", "2022-01-01", "Prueba", "Prueba", "Prueba", 1000, 1, 1, 1, 1, 1, 1, 1, 1),
+    //createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
+    //createData("Eclair", 262, 16.0, 24, 6.0),
+    //createData("Cupcake", 305, 3.7, 67, 4.3),
+    //createData("Gingerbread", 356, 16.0, 49, 3.9),
   ];
+
+  
 
   /*   function formatDate(date) {
     var d = new Date(date),
@@ -97,32 +102,6 @@ export const OrdenesVentaGestionPage = () => {
       setFechaVisita(null);
     }
   }
-
-  /* function handleChangeFechaDocumento(value) {
-    //console.log(formatDate(value));
-    setFechaDocumento(formatDate(value));
-  } */
-
-  /*   function handleChangeFechaVisita(value) {
-    //console.log(formatDate(value));
-    setFechaVisita(formatDate(value));
-  } */
-
-  /*  function handleTipoServicioChange(event) {
-    //...
-    //console.log(event.target.value);
-    setTipoServicio(event.target.value);
-  } */
-
-  /*   function handleDescripcionChange(event) {
-    //console.log(event.target.value);
-    setDescripcion(event.target.value);
-  } */
-
-  /*   function handlePersonalChange(event) {
-    //console.log(event.target.value);
-    setTecnico(event.target.value);
-  } */
 
   function handleChangeClienteValue(event, value) {
     setClienteValue(value.id);
@@ -337,6 +316,7 @@ export const OrdenesVentaGestionPage = () => {
   const [marcas, setMarcas] = useState([]);
   const [tipoMaterial, setTipoMaterial] = useState([]);
   const [fechaAlta, setFechaAlta] = useState(todayDate);
+  
 
   let { orderId } = useParams();
   console.log("id recibido " + orderId);
@@ -383,6 +363,9 @@ export const OrdenesVentaGestionPage = () => {
         //console.log("orden de venta cargada " + JSON.stringify(ordenVenta));
       }
     }
+
+    setDetalleOrdenVenta(rows);
+    console.log(detalleOrdenVenta)
   }, [ord]);
 
   return (
@@ -494,26 +477,26 @@ export const OrdenesVentaGestionPage = () => {
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
         <TableHead>
           <TableRow>
-            <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell align="right">Calories</TableCell>
-            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+            <TableCell align="right">Obsevacion</TableCell>
+            <TableCell align="right">Descripción</TableCell>
+            <TableCell align="right">No serie</TableCell>
+            <TableCell align="right">Cantidad</TableCell>
+            <TableCell align="right">Monto</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
           {rows.map((row) => (
             <TableRow
-              key={row.name}
+              key={row.id}
               sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
             >
-              <TableCell component="th" scope="row">
-                {row.name}
+              <TableCell align="right" component="th" scope="row">
+                {row.observacion_1}
               </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-              <TableCell align="right">{row.carbs}</TableCell>
-              <TableCell align="right">{row.protein}</TableCell>
+              <TableCell align="right">{row.descripcion}</TableCell>
+              <TableCell align="right">{row.num_serie}</TableCell>
+              <TableCell align="right">{row.cantidad}</TableCell>
+              <TableCell align="right">{row.monto}</TableCell>
             </TableRow>
           ))}
         </TableBody>
